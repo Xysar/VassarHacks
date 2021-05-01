4fbcf49d65f7ead14fdaa73d574fa8ff63098fdc
Xysar <cesarcamacho760@gmail.com>
2021-05-01 16:51:13 -0400

    Update Whiteboard.java

--- src/Whiteboard.java
+++ src/Whiteboard.java
@@ -1,2 +1,39 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
 public class Whiteboard {
+    JFrame board;
+    JPanel toolP;
+    JButton pencilB;
+    Border loweredetched;
+
+    public Whiteboard(){
+        this.toolP = new JPanel();
+        this.loweredetched = BorderFactory.createEtchedBorder(1);
+        this.pencilB = new JButton("Pencil");
+
+        this.initialize();
+    }
+
+    public void initialize(){
+        this.board = new JFrame();
+
+        this.board.setResizable(false);
+        this.board.setSize(850, 650);
+        this.board.setTitle("White board");
+        this.board.setDefaultCloseOperation(3);
+        this.board.getContentPane().setLayout(null);
+        this.toolP.setBorder(BorderFactory.createTitledBorder(this.loweredetched, "Palette"));
+        this.toolP.setLayout((LayoutManager)null);
+        this.board.getContentPane().add(toolP);
+        this.toolP.add(pencilB);
+        this.toolP.setBounds(10, 10, 200, 600);
+        this.pencilB.setBounds(40,50,120,25);
+        this.board.setVisible(true);
+
+    }
+    public static void main(String[] args){
+        new Whiteboard();
+    }
 }
