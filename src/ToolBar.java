5beeba653226549b7ed5f043c4a7d971888dbf8e
sracedorfman <83509848+sracedorfman@users.noreply.github.com>
2021-05-01 19:28:43 -0400

    Revert "Update ToolBar.java"
    This reverts commit 0191c1847eb69eb942c643ebc4dc162dc9403bf4.

--- src/ToolBar.java
+++ src/ToolBar.java
@@ -6,103 +6,57 @@ import java.awt.event.ActionListener;
 public class ToolBar extends JPanel{
     public static final int PENCIL = 0;
     public static final int ERASER = 1;
-    public static final int FREEDRAW = 0;
-    public static final int LINEDRAW = 1;
-    private final String[] colorNames;
 
     private JButton pencilB;
     private JPanel sizeP;
-    private JLabel sizeL;
     private JButton sizeDown;
     private JLabel size;
     private JButton sizeUp;
     private JButton eraserB;
-    private JComboBox<String> colorBox;
-    private JButton freeDrawB;
-    private JButton lineB;
 
     private ClickListener listener;
-
-    //for tracking which buttons to gray out
-    private JButton toolB;
-    private JButton drawStyleB;
 
     public ToolBar() {
         listener = new ClickListener();
 
-        //component initializations
         pencilB = new JButton("Pencil");
         sizeP = new JPanel();
-        sizeL = new JLabel("Brush Size:", JLabel.CENTER);
         sizeDown = new JButton("-");
         size = new JLabel("1", JLabel.CENTER);
         sizeUp = new JButton("+");
         eraserB = new JButton("Eraser");
-        colorBox = new JComboBox<>(colorNames);
-        freeDrawB = new JButton("Free Draw");
-        lineB = new JButton("Line");
 
-        //panel setup
-        this.setLayout(null);
+        //this.setPreferredSize(new Dimension(200,600));
+        this.setLayout((LayoutManager)null);
         this.setBounds(10, 10, 200, 600);
 
         pencilB.setBounds(40,50,120,25);
         pencilB.addActionListener(listener);
-        pencilB.setEnabled(false);
 
-        eraserB.setBounds(40, 80, 120, 25);
-        eraserB.addActionListener(listener);
-
-        sizeP.setLayout(null);
-        sizeP.setBounds(40, 130, 120, 50);
-        sizeL.setBounds(0, 0, 120, 25);
-        sizeDown.setBounds(0, 25, 30, 25);
+        sizeP.setLayout((LayoutManager) null);
+        sizeP.setBounds(40, 100, 120, 25);
+        sizeDown.setBounds(0, 0, 25, 25);
         sizeDown.addActionListener(listener);
-        size.setBounds(30, 25, 60, 25);
-        sizeUp.setBounds(90, 25, 30, 25);
+        size.setBounds(25, 0, 70, 25);
+        sizeUp.setBounds(95, 0, 25, 25);
         sizeUp.addActionListener(listener);
-        sizeP.add(sizeL);
         sizeP.add(sizeDown);
         sizeP.add(size);
         sizeP.add(sizeUp);
 
+        eraserB.setBounds(40, 150, 120, 25);
+        eraserB.addActionListener(listener);
 
-
-        colorBox.setBounds(40, 190, 120, 25);
-        colorBox.addActionListener(listener);
-        colorBox.setActionCommand("colorSelected");
-
-        freeDrawB.setBounds(40, 240, 120, 25);
-        lineB.setBounds(40, 270, 120, 25);
-        freeDrawB.addActionListener(listener);
-        lineB.addActionListener(listener);
-        freeDrawB.setEnabled(false);
-
-        //adding components
         this.add(pencilB);
         this.add(sizeP);
         this.add(eraserB);
-        this.add(colorBox);
-        this.add(freeDrawB);
-        this.add(lineB);
-
-        toolB = pencilB;
-        drawStyleB = freeDrawB;
     }
 
     public void setTool(int tool) {
-        if (tool == PENCIL) {
-            toolB.setEnabled(true);
-            toolB = pencilB;
-            toolB.setEnabled(false);
+        if (tool == PENCIL)
             System.out.println("Using the pencil");
-        }
-        else if (tool == ERASER) {
-            toolB.setEnabled(true);
-            toolB = eraserB;
-            toolB.setEnabled(false);
+        else if (tool == ERASER)
             System.out.println("Using the eraser");
-        }
     }
 
     public void decreaseSize() {
@@ -113,27 +67,6 @@ public class ToolBar extends JPanel{
     public void increaseSize() {
         size.setText((Integer.parseInt(size.getText()) + 1) + "");
         System.out.println("Size increased");
-    }
-
-    //method to handle a new color selection
-    public void setColor() {
-        System.out.println("Using " + ((String) colorBox.getSelectedItem()).toLowerCase());
-    }
-
-    //method to handle setting draw method
-    public void setDrawMethod(int draw) {
-        if (draw == FREEDRAW) {
-            drawStyleB.setEnabled(true);
-            drawStyleB = freeDrawB;
-            drawStyleB.setEnabled(false);
-            System.out.println("Drawing style set to free draw");
-        }
-        else if (draw == LINEDRAW) {
-            drawStyleB.setEnabled(true);
-            drawStyleB = lineB;
-            drawStyleB.setEnabled(false);
-            System.out.println("Drawing style set to line drawing");
-        }
     }
 
     private class ClickListener implements ActionListener {
@@ -147,12 +80,6 @@ public class ToolBar extends JPanel{
                 decreaseSize();
             else if (e.getActionCommand().equals("+"))
                 increaseSize();
-            else if (e.getActionCommand().equals("colorSelected"))
-                setColor();
-            else if (e.getActionCommand().equals("Free Draw"))
-                setDrawMethod(FREEDRAW);
-            else if (e.getActionCommand().equals("Line"))
-                setDrawMethod(LINEDRAW);
         }
     }
 }
